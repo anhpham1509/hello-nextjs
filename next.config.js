2b717ebbf2280f9bbb50a3d7255f3b0bdee95df9
Anh Pham <duyanhpham1509@gmail.com>
2019-03-08 16:21:34 +0200

    feat: export static html and deployments to now.

--- next.config.js
+++ next.config.js
@@ -1,3 +1,12 @@
 module.exports = {
-  target: 'serverless'
+  exportPathMap: () => {
+    return {
+      '/': {page: '/'},
+      '/about': {page: '/about'},
+      '/post/hello-nextjs': {page: '/post', query: {title: 'Hello Next.js'}},
+      '/post/learn-nextjs': {page: '/post', query: {title: 'Learn Next.js is awesome'}},
+      '/post/deploy-nextjs': {page: '/post', query: {title: 'Deploy apps with Zeit'}},
+      '/post/exporting-pages': {page: '/post', query: {title: 'Learn to Export HTML Pages'}}
+    };
+  }
 };
